32cd79b4186d2e4dc506d457c0b5307a8c3cb6b8
dsocia <dsocia22@gmail.com>
2019-10-21 13:14:43 -0400

    Potential Field

--- ObstacleAvoid.m
+++ ObstacleAvoid.m
@@ -1,11 +1,12 @@
 clc
+% clear
 
 start = [2 2 -2];
 goal = [20 20 20];
 
-x = -5:.5:22;
-y = -5:.5:22;
-z = -5:.5:22;
+xf = linspace(-5, 22, 10);
+yf = linspace(-5, 22, 10);
+zf = linspace(-5, 22, 10);
 
 % field = 
 
@@ -13,7 +14,7 @@ z = -5:.5:22;
 attConst = 5;
 
 % repulsive constant
-repConst = 2;
+repConst = 10;
 
 %% Plot obstacles
 %sphere center and radius
@@ -21,10 +22,10 @@ spC1 = [3 3 3];
 r1 =2;
 
 spC2 = [15 5 0];
-r2 = 3;
+r2 = 5;
 
 spC3 = [11 15 12];
-r3 = 4;
+r3 = 3;
 
 obsCenters = [spC1; spC2; spC3];
 obsRadius = [r1, r2, r3];
@@ -43,9 +44,42 @@ plot3(start(1), start(2), start(3), 'ko')
 % plot goal
 plot3(goal(1), goal(2), goal(3), 'R*')
 
+pointField = [];
+attPotential = [];
+for i = xf
+    for j = yf
+       for k = zf
+          pt = [i j k];
+          pointField = [pointField; pt];
+          attpot = att_components(attConst, pt, goal);
+          attPotential = [attPotential; attpot];
+       end
+    end
+end
+% 
+repPotential = zeros(size(pointField, 1), 3);
+for i  = 1:size(obsCenters)
+    cent = obsCenters(i, :)
+    r = obsRadius(i)
+    repField = [];
+    for k = 1:size(pointField, 1)
+        
+        reppot = rep_components(repConst, pointField(k, :), cent, r);
+        repField = [repField; reppot];
+    end
+    repPotential = repPotential + repField;
+end
 
+potential = attPotential + repPotential*10;
+% potential = repPotential * 100;
+% potential = repPotential*100;
+potential = normalize(potential, 'range');
 
-
+quiver3(pointField(:,1), pointField(:,2), pointField(:,3), potential(:, 1), potential(:, 2), potential(:, 3))
+% quiver(pointField(:,1), pointField(:,2), potential(:, 1), potential(:, 2))
+% 
+% c = q.Color;
+% q.Color = 'black';
 
 %% Methods
 % function to calculate distance between a sphere and a point 
@@ -58,16 +92,37 @@ function dist = eucliDist(point, target)
 end
 
 function u_att = attractive_potetial(currPt, goal, attConst)
-    u_att = 0.5 * attConst * sum((currPt - goal)^2);
+    u_att = 0.5 * attConst * sum((currPt - goal).^2);
 end
 
-function [x_att, y_att, z_att] = att_compontents(attConst, currPt, goal)
+function potential_components = att_components(attConst, currPt, goal)
     x_att = -attConst * (currPt(1) - goal(1));
     y_att = -attConst * (currPt(2) - goal(2));
     z_att = -attConst * (currPt(3) - goal(3));
+    
+    potential_components = [x_att y_att z_att];
 end
 
-function u_rep = repulsive_potential(currPt, obs, radius, repConst)
-    u_rep = 0.5 * ((repConst * radius)/sum((currPt - obs)^2));
+function u_rep = repulsive_potential(x, y, z, obs, radius, repConst)
+    u_rep = 0.5 * ((repConst * radius)/((x-obs(1))^2 + (y-obs(2))^2 + (z-obs(3)^2)));
 end
 
+% Repulsive forces
+function potential_components = rep_components(repConst, currPt, obs, radius)
+   syms x y z
+%    u_rep = 0.5 * ((repConst * radius)/((x-obs(1))^2 + (y-obs(2))^2 + (z-obs(3))^2));
+   u_rep = 0.5 *  ((repConst)/((x-obs(1))^2 + (y-obs(2))^2 + (z-obs(3))^2));
+   x_rep = -radius^2 *diff(u_rep, x);
+   y_rep = -radius^2 *diff(u_rep, y);
+   z_rep = -radius^2 *diff(u_rep, z);
+   
+   x = currPt(1);
+   y = currPt(2);
+   z = currPt(3);
+   
+   if currPt == obs
+      potential_components = [0 0 0];
+   else
+      potential_components = [double(subs(x_rep)) double(subs(y_rep)) double(subs(z_rep))]; 
+   end
+end
