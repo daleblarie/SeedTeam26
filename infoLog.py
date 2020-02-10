a03bafa40fdc43a5515a5bbb06942806ac26ac62
daleblarie <DaleBLarie@gmail.com>
2020-02-10 13:14:12 -0500

    added kalman stuff and write to CSV

--- infoLog.py
+++ infoLog.py
@@ -66,6 +66,7 @@ class LoggingExample:
 
     def __init__(self, link_uri, droneNum):
         """ Initialize and run the example with the specified link_uri """
+        self.is_connected = False
 
         self._cf = Crazyflie(rw_cache='./cache')
 
@@ -82,7 +83,6 @@ class LoggingExample:
         self.link_uri = link_uri
 
         # Variable used to keep main loop occupied until disconnect
-        self.is_connected = True
         self.droneNum = droneNum
 
     def _connected(self, link_uri):
@@ -91,25 +91,54 @@ class LoggingExample:
         print('Connected to %s' % link_uri)
         try:
             os.mkdir("testCSV/" + str(self.droneNum))
-        except:
-            pass
+        except Exception as e:
+            print(e)
 
         try:
             print("openingFiles")
-            f = open("/testCSV/" + str(droneNum) + '/stab.csv')
-            f.write("stabilizer.roll, stabilizer.pitch, stabilizer.yaw")
+            f = open("./testCSV/" + str(self.droneNum) + "/stab.csv", 'w')
+            f.write("stabilizer.roll, stabilizer.pitch, stabilizer.yaw\n")
             f.close()
             print("created stab")
+        except Exception as e:
+            print("stab csv not created")
+            print(e)
+            return
 
-            f = open("/testCSV/" + str(droneNum) + '/gyro.csv')
-            f.write("gyro.x, gyro.y, gyro.z")
-            f.close()
-
-            f = open("/testCSV/" + str(droneNum) + '/accel.csv')
-            f.write("acc.x, acc.y, acc.z")
+        try:
+            f = open("./testCSV/" + str(self.droneNum) + "/gyro.csv", 'w')
+            f.write("gyro.x, gyro.y, gyro.z\n")
             f.close()
         except:
-            pass
+            print("gyro csv not created")
+
+        try:
+            f = open("./testCSV/" + str(self.droneNum) + '/accel.csv', 'w')
+            f.write("acc.x, acc.y, acc.z\n")
+            f.close()
+        except:
+            print("acc csv not created")
+
+        try:
+            f = open("./testCSV/" + str(self.droneNum) + '/kalmanState.csv', 'w')
+            f.write("kalman.stateX, kalman.stateY, kalman.stateZ\n")
+            f.close()
+        except:
+            print("kalman csv not created")
+
+        try:
+            f = open("./testCSV/" + str(self.droneNum) + '/kalmanPos.csv', 'w')
+            f.write("kalman.statePX, kalman.statePY, kalman.statePZ\n")
+            f.close()
+        except:
+            print("kalman csv not created")
+
+        try:
+            f = open("./testCSV/" + str(self.droneNum) + '/kalmanD.csv', 'w')
+            f.write("kalman.stateD0, kalman.stateD1, kalman.stateD2\n")
+            f.close()
+        except:
+            print("kalman csv not created")
 
         # The definition of the logconfig can be made before connecting
         self._lg_stab = LogConfig(name='stab', period_in_ms=100)
@@ -128,6 +157,22 @@ class LoggingExample:
         self._lg_accel.add_variable('acc.y', 'float')
         self._lg_accel.add_variable('acc.z', 'float')
 
+        self._lg_kalmanState = LogConfig(name='kalmanState', period_in_ms=100)
+        self._lg_kalmanState.add_variable('kalman.stateX', 'float')
+        self._lg_kalmanState.add_variable('kalman.stateY', 'float')
+        self._lg_kalmanState.add_variable('kalman.stateZ', 'float')
+
+        self._lg_kalmanPos = LogConfig(name='kalmanPos', period_in_ms=100)
+        self._lg_kalmanPos.add_variable('kalman.statePX', 'float')
+        self._lg_kalmanPos.add_variable('kalman.statePY', 'float')
+        self._lg_kalmanPos.add_variable('kalman.statePZ', 'float')
+
+        self._lg_kalmanD = LogConfig(name='kalmanD', period_in_ms=100)
+        self._lg_kalmanD.add_variable('kalman.stateD0', 'float')
+        self._lg_kalmanD.add_variable('kalman.stateD1', 'float')
+        self._lg_kalmanD.add_variable('kalman.stateD2', 'float')
+
+
 
 
 
@@ -138,18 +183,30 @@ class LoggingExample:
             self._cf.log.add_config(self._lg_stab)
             self._cf.log.add_config(self._lg_gyro)
             self._cf.log.add_config(self._lg_accel)
+            self._cf.log.add_config(self._lg_kalmanState)
+            self._cf.log.add_config(self._lg_kalmanPos)
+            self._cf.log.add_config(self._lg_kalmanD)
             # This callback will receive the data
             self._lg_stab.data_received_cb.add_callback(self._stab_log_data)
             self._lg_gyro.data_received_cb.add_callback(self._stab_log_data)
             self._lg_accel.data_received_cb.add_callback(self._stab_log_data)
+            self._lg_kalmanState.data_received_cb.add_callback(self._stab_log_data)
+            self._lg_kalmanPos.data_received_cb.add_callback(self._stab_log_data)
+            self._lg_kalmanD.data_received_cb.add_callback(self._stab_log_data)
             # This callback will be called on errors
             self._lg_stab.error_cb.add_callback(self._stab_log_error)
             self._lg_gyro.error_cb.add_callback(self._stab_log_error)
             self._lg_accel.error_cb.add_callback(self._stab_log_error)
+            self._lg_kalmanState.error_cb.add_callback(self._stab_log_error)
+            self._lg_kalmanPos.error_cb.add_callback(self._stab_log_error)
+            self._lg_kalmanD.error_cb.add_callback(self._stab_log_error)
             # Start the logging
             self._lg_stab.start()
             self._lg_gyro.start()
             self._lg_accel.start()
+            self._lg_kalmanState.start()
+            self._lg_kalmanPos.start()
+            self._lg_kalmanD.start()
         except KeyError as e:
             print('Could not start log configuration,'
                   '{} not found in TOC'.format(str(e)))
@@ -157,6 +214,7 @@ class LoggingExample:
             print('Could not add Stabilizer log config, bad configuration.')
 
         # Start a timer to disconnect in 10s
+        self.connected = True
         t = Timer(5, self._cf.close_link)
         t.start()
 
@@ -169,6 +227,7 @@ class LoggingExample:
         print('[%d][%s]: %s' % (timestamp, logconf.name, data))
         with open('testCSV/' + str(self.droneNum)+ "/" + logconf.name + '.csv','a') as fd:
             fd.write(convertDictToStr(data) + "\n")
+            fd.close()
 
     def _connection_failed(self, link_uri, msg):
         """Callback when connection initial connection fails (i.e no Crazyflie
@@ -207,4 +266,4 @@ if __name__ == '__main__':
     # so this is where your application should do something. In our case we
     # are just waiting until we are disconnected.
     while le.is_connected:
-        time.sleep(1)
+        time.sleep(10)
