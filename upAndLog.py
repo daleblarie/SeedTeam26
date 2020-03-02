214b20e5ebba0f838e65272fdcbb12f30c5dd0fa
daleblarie <DaleBLarie@gmail.com>
2020-03-02 12:17:39 -0500

    updtaing everything ith infolog

--- upAndLog.py
+++ upAndLog.py
@@ -42,6 +42,8 @@ from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 
+from infoLog import LoggingExample
+
 # URI to the Crazyflie to connect to
 uri = 'radio://0/80/2M'
 
@@ -49,26 +51,12 @@ uri = 'radio://0/80/2M'
 #             x    y    z
 sequence = [
     (0, 0, 0.7),
-    (1, 0, 0.7),
-    (1, 1, 0.7),
-    (0, 1, 0.7),
-    (0, 0, 0.7),
-    (0, 0, 1.7),
-    (1, 0, 1.7),
-    (1, 1, 1.7),
-    (0, 1, 1.7),
-    (0, 0, 1.7),
-    (0, 0, 0.1)
 ]
 
 
-def wait_for_position_estimator(scf):
+def wait_for_position_estimator(scf, logger):
     print('Waiting for estimator to find position...')
 
-    log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
-    log_config.add_variable('kalman.varPX', 'float')
-    log_config.add_variable('kalman.varPY', 'float')
-    log_config.add_variable('kalman.varPZ', 'float')
 
     var_y_history = [1000] * 10
     var_x_history = [1000] * 10
@@ -76,53 +64,52 @@ def wait_for_position_estimator(scf):
 
     threshold = 0.001
 
-    with SyncLogger(scf, log_config) as logger:
-        for log_entry in logger:
-            data = log_entry[1]
+    for log_entry in logger:
+        data = log_entry[1]
 
-            var_x_history.append(data['kalman.varPX'])
-            var_x_history.pop(0)
-            var_y_history.append(data['kalman.varPY'])
-            var_y_history.pop(0)
-            var_z_history.append(data['kalman.varPZ'])
-            var_z_history.pop(0)
+        var_x_history.append(data['kalman.varPX'])
+        var_x_history.pop(0)
+        var_y_history.append(data['kalman.varPY'])
+        var_y_history.pop(0)
+        var_z_history.append(data['kalman.varPZ'])
+        var_z_history.pop(0)
 
-            min_x = min(var_x_history)
-            max_x = max(var_x_history)
-            min_y = min(var_y_history)
-            max_y = max(var_y_history)
-            min_z = min(var_z_history)
-            max_z = max(var_z_history)
+        min_x = min(var_x_history)
+        max_x = max(var_x_history)
+        min_y = min(var_y_history)
+        max_y = max(var_y_history)
+        min_z = min(var_z_history)
+        max_z = max(var_z_history)
 
             # print("{} {} {}".
             #       format(max_x - min_x, max_y - min_y, max_z - min_z))
 
-            if (max_x - min_x) < threshold and (
-                    max_y - min_y) < threshold and (
-                    max_z - min_z) < threshold:
-                break
+        if (max_x - min_x) < threshold and (
+                max_y - min_y) < threshold and (
+                max_z - min_z) < threshold:
+            break
 
 
-def set_initial_position(scf, x, y, z, yaw_deg):
-    scf.cf.param.set_value('kalman.initialX', x)
-    scf.cf.param.set_value('kalman.initialY', y)
-    scf.cf.param.set_value('kalman.initialZ', z)
+def set_initial_position(cfIn, x, y, z, yaw_deg):
+    cfIn.param.set_value('kalman.initialX', x)
+    cfIn.param.set_value('kalman.initialY', y)
+    cfIn.param.set_value('kalman.initialZ', z)
 
-    yaw_radians = math.radians(yaw_deg)
-    scf.cf.param.set_value('kalman.initialYaw', yaw_radians)
+    # yaw_radians = math.radians(yaw_deg)
+    # scf.cf.param.set_value('kalman.initialYaw', yaw_radians)
 
 
-def reset_estimator(scf):
-    cf = scf.cf
+def reset_estimator(cfIn, logger):
+    cf = cfIn
     cf.param.set_value('kalman.resetEstimation', '1')
     time.sleep(0.1)
     cf.param.set_value('kalman.resetEstimation', '0')
 
-    wait_for_position_estimator(cf)
+    wait_for_position_estimator(cf, logger)
 
 
-def run_sequence(scf, sequence, base_x, base_y, base_z, yaw):
-    cf = scf.cf
+def run_sequence(cfIn, sequence, base_x, base_y, base_z, yaw):
+    cf = cfIn
 
     for position in sequence:
         print('Setting position {}'.format(position))
@@ -143,20 +130,30 @@ def run_sequence(scf, sequence, base_x, base_y, base_z, yaw):
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers(enable_debug_driver=False)
+    print('Scanning interfaces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+    print('Crazyflies found:')
+    for i in available:
+        print(i[0])
 
-    # Set these to the position and yaw based on how your Crazyflie is placed
-    # on the floor
-    initial_x = 1.0
-    initial_y = 1.0
-    initial_z = 0.0
-    initial_yaw = 0  # In degrees
-    # 0: positive X direction
-    # 90: positive Y direction
-    # 180: negative X direction
-    # 270: negative Y direction
+    if len(available) > 0:
+        le = LoggingExample(available[0][0], 'drone1')
 
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        set_initial_position(scf, initial_x, initial_y, initial_z, initial_yaw)
-        reset_estimator(scf)
-        run_sequence(scf, sequence,
-                     initial_x, initial_y, initial_z, initial_yaw)
+        # Set these to the position and yaw based on how your Crazyflie is placed
+        # on the floor
+        initial_x = 1.0
+        initial_y = 1.0
+        initial_z = 0.0
+        initial_yaw = 0  # In degrees
+        # 0: positive X direction
+        # 90: positive Y direction
+        # 180: negative X direction
+        # 270: negative Y direction
+
+        # with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        # set_initial_position(le._cf, initial_x, initial_y, initial_z, initial_yaw)
+        # reset_estimator(le._cf, le)
+        run_sequence(le._cf, sequence, initial_x, initial_y, initial_z, initial_yaw)
+        time.sleep(50)
+    else:
+        print('No Crazyflies found, cannot run example')
